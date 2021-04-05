4c1b7752a4c0f5dd59728d72bc10a91261582795
n-villate <n-villate@javeriana.edu.co>
2021-04-04 19:16:51 -0500

    Creación Punto 5
    Sensores de Luminosidad

--- app/src/main/java/com/example/taller2/MapsActivity.java
+++ app/src/main/java/com/example/taller2/MapsActivity.java
@@ -11,6 +11,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,6 +37,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.taller2.databinding.ActivityMapsBinding;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -47,8 +52,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     LocationRequest locationRequest;
     LocationCallback locationCallback;
 
+    //Permisos
     String permLocation = Manifest.permission.ACCESS_FINE_LOCATION;
     private static final int LOCATION_PERMISSION_ID = 15;
+
+    //Sensores
+    SensorManager sensorManager;
+    Sensor lightSensor;
+    SensorEventListener lightSensorListener;
 
     //Settings
     private static final int SETTINGS_GPS = 20;
@@ -59,6 +70,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //Sensores de Luminosidad
+        initLightSensor();
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -89,6 +103,31 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         initView();
     }
 
+    private void initLightSensor() {
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        lightSensorListener = new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                if (mMap != null) {
+                    if (event.values[0] < 5000) {
+                        Log.i("MAPS", "DARK MAP " + event.values[0]);
+                        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MapsActivity.this, R.raw.darkmap));
+                    }
+                    else {
+                        Log.i("MAPS", "LIGHT MAP " + event.values[0]);
+                        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(MapsActivity.this, R.raw.lightmap));
+                    }
+                }
+            }
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+    }
+
     private void initView(){
         if(ContextCompat.checkSelfPermission(this, permLocation)== PackageManager.PERMISSION_GRANTED) {
             checkSettingsLocation();
@@ -100,6 +139,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
         mMap.getUiSettings().setZoomGesturesEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        mMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.lightmap));
 
     }
 
@@ -127,12 +168,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onResume() {
         super.onResume();
+        sensorManager.registerListener(lightSensorListener, lightSensor, sensorManager.SENSOR_DELAY_NORMAL);
         startLocationUpdates();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        sensorManager.unregisterListener(lightSensorListener);
         stopLocationUpdates();
     }
 
